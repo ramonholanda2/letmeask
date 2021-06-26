570e6954e92097ccb227d9a167edf6c4d40240d8
ramonsilva2 <ramonsilva.silva.rsrs@gmail.com>
2021-06-26 16:37:43 -0300

    config

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -5,14 +5,14 @@ import "firebase/auth";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyBogvQ-i6t3kr2zBmrfJfaEJfMeZ7IjYTg",
+  authDomain: "letmeask-88518.firebaseapp.com",
+  databaseURL: "https://letmeask-88518-default-rtdb.firebaseio.com",
+  projectId: "letmeask-88518",
+  storageBucket: "letmeask-88518.appspot.com",
+  messagingSenderId: "731345867260",
+  appId: "1:731345867260:web:cba81c6a2c2fcdb2ab6279",
+  measurementId: "G-W2875HF2TZ"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
